ed918fcbc686501f8c87ca8dd41b7fdc70981621
Cerunnos <github email address>
2018-03-07 13:02:05 -0500

    remove backend

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const column = document.getElementById("column")
-  const mainColumn = document.getElementById("mainColumn")
-
-  // $(document).ready(function() {
-  //   $("testButton").click(function() {
-  //     $('.test.modal').modal('show');
-  //   });
-  // });
+  // const column = document.getElementById("column")
+  // const columnOne = document.getElementById("columnOne")
+  let modalContent = document.getElementById("modalContent")
 
   fetchRestaurants()
 
@@ -17,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function displayRestaurants(json) {
+    let counter = 0
     for (let i = 0; i < json.length; i++) {
       const div = document.createElement("div")
       div.setAttribute("class", "ui card")
@@ -24,9 +20,12 @@ document.addEventListener("DOMContentLoaded", function() {
       const name = json[i].name
       const cuisine = json[i].cuisine
       const location = json[i].location
+      const img = json[i].img_url
+      console.log(img)
       div.innerHTML =
-        `<div class="card">
+        `<div class="ui fluid card">
         <div class="content">
+        <img src="./pictures/${img}" class="visible content">
           <div class="header">
             ${name}
           </div>
@@ -34,26 +33,28 @@ document.addEventListener("DOMContentLoaded", function() {
             ${cuisine}
           </div>
           <div class="description">
-          <span class="ui label">
-            PHOTO
-          </span>
             ${location}
           </div>
-        </div>
-        <div class="extra content">
-        <div id=${json[i].id} class="ui blue button">Order</div>
-        </div>
       </div>`
-      mainColumn.appendChild(div)
+      if (counter === 0) {
+        columnOne.appendChild(div)
+        counter++
+      } else if (counter === 1) {
+        columnTwo.appendChild(div)
+        counter++
+      } else if (counter === 2) {
+        columnThree.appendChild(div)
+        counter++
+      } else {
+        columnFour.appendChild(div)
+        counter = 0
+      }
+      $('.ui.modal.1').modal('attach events', '.ui.button', 'show')
+      // div.appendChild(img)
       button = document.getElementById(json[i].id)
       button.addEventListener("click", event => {
         event.preventDefault()
         fetchItems(event)
-        // $(document).ready(function() {
-        //   $("testButton").click(function() {
-        //     $('.test.modal').modal('show');
-        //   });
-        // });
       })
     }
   }
@@ -69,18 +70,14 @@ document.addEventListener("DOMContentLoaded", function() {
     for (i = 0; i < json.length; i++) {
       if (json[i].restaurant_id == event.target.id) {
         const div = document.createElement("div")
-        div.innerHTML = json[i].name
-        mainColumn.appendChild(div)
+        div.innerText += json[i].name
+        modalContent.innerHTML = ""
+        modalContent.appendChild(div)
+        console.log(div)
       }
     }
   }
 })
-
-$(document).ready(function() {
-  $("testButton").click(function() {
-    $('.test.modal').modal('show');
-  });
-});
 
 
 
